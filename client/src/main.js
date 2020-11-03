f5292ab189d7d282129b9efd3552a84ed2511562
narwhal99 <molnarandre@outlook.com>
2020-11-03 13:13:37 +0100

    init

--- client/src/main.js
+++ client/src/main.js
@@ -1,4 +1,14 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify';
+import store from './store'
+import router from './router'
 
-createApp(App).mount('#app')
+Vue.config.productionTip = false
+
+new Vue({
+  vuetify,
+  store,
+  router,
+  render: h => h(App)
+}).$mount('#app')
